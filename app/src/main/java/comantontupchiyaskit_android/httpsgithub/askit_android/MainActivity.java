fd130832ffbc96ec2eb578a2dfd9dad668667309
beli3v3inm3 <zhynkin.vitaliy@gmail.com>
2015-04-06 23:47:14 +0300

    TEST

--- app/src/main/java/comantontupchiyaskit_android/httpsgithub/askit_android/MainActivity.java
+++ app/src/main/java/comantontupchiyaskit_android/httpsgithub/askit_android/MainActivity.java
@@ -8,7 +8,7 @@ import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
-    //Test
+    //TestTYT
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
