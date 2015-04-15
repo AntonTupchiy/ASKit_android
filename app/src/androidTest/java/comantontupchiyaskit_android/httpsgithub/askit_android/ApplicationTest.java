e6739ca54d01a2e9fca610c2f3082f3a914ef1a6
beli3v3inm3 <zhynkin.vitaliy@gmail.com>
2015-04-15 16:13:48 +0300

    hardcode fix

--- app/src/androidTest/java/comantontupchiyaskit_android/httpsgithub/askit_android/ApplicationTest.java
+++ app/src/androidTest/java/comantontupchiyaskit_android/httpsgithub/askit_android/ApplicationTest.java
@@ -1,13 +1,14 @@
 package comantontupchiyaskit_android.httpsgithub.askit_android;
 
 import android.app.Application;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.test.ApplicationTestCase;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
-public class ApplicationTest extends ApplicationTestCase<Application> {
-    public ApplicationTest() {
+//public class ApplicationTest extends SQLiteOpenHelper {
+/*    public ApplicationTest() {
         super(Application.class);
-    }
-}
+    }*/
+//}
--- app/src/main/java/comantontupchiyaskit_android/httpsgithub/askit_android/MainActivity.java
+++ app/src/main/java/comantontupchiyaskit_android/httpsgithub/askit_android/MainActivity.java
@@ -12,10 +12,8 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.registration);
     }
-
-    // ВИТАЛЯ ЛОХ
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
