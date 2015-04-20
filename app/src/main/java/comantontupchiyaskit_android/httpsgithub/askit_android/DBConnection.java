43068f9a45edde245fb95bf34563582d0542cc99
beli3v3inm3 <zhynkin.vitaliy@gmail.com>
2015-04-20 14:57:56 +0300

    upd

--- app/src/main/java/comantontupchiyaskit_android/httpsgithub/askit_android/DBConnection.java
+++ app/src/main/java/comantontupchiyaskit_android/httpsgithub/askit_android/DBConnection.java
@@ -11,48 +11,53 @@ import java.sql.Statement;
  * Created by user on 15.04.2015.
  */
 public class DBConnection {
-    private final String LOG_TAG = "myLogs";
+    private static final String LOG_TAG = "myLogs";
     private static String Url = "jdbc:jtds:sqlserver://208.118.63.109:1433;databaseName=DB_9BD928_AskIt;";
     private static String Login = "DB_9BD928_AskIt_admin";
     private static String Password = "q1w2E3r4";
     private static String driver = "net.sourceforge.jtds.jdbc.Driver";
-    private Connection  con = null;
+    private static Connection con = null;
     private Statement stmt = null;
     private String sql = "INSERT INTO [dbo].[Users] ([Login], [Name], [Surname], [E-Mail], [Password], [Knowledge]) " +
-            "VALUES ('vasya','vasya','vasya','vasya@mail.ru','vasya', 'vasya')";
+            "VALUES ('vasya','vasya','vasya','vasya@google.ru','vasya', 'vasya')";
 
-    //init
-
-    public void mysqlInit() {
-        Log.d(LOG_TAG,"-------- MySQL JDBC Connection Testing ------------");
-
+    //Connection to mssql database server
+    private static Connection getDBConnection() {
         try {
             Class.forName(driver);
+
         } catch (ClassNotFoundException e) {
-            Log.d(LOG_TAG,"Where is your MySQL JDBC Driver?");
+            Log.d(LOG_TAG, "Where is your MySQL JDBC Driver?");
             e.printStackTrace();
-            return;
         }
 
         Log.d(LOG_TAG,"MySQL JDBC Driver Registered!");
+
+        try {
+            con = DriverManager.getConnection(Url, Login, Password);
+            return con;
+
+        } catch (Exception e) {
+            Log.d(LOG_TAG, "Connection Failed! Check output console");
+            e.printStackTrace();
+        }
+        return con;
+    }
+
+    //Insert query to mssql
+    public void mysqlInit() {
 
         Thread thread = new Thread(new Runnable(){
             @Override
             public void run() {
                 try {
-                    con = DriverManager.getConnection(Url, Login, Password);
+                    con = getDBConnection();
                     stmt = con.createStatement();
                     stmt.execute(sql);
                 } catch (Exception e) {
-                    Log.d(LOG_TAG, "Connection Failed! Check output console");
+                    Log.d(LOG_TAG, "Connection or Execution Failed! Check output console");
                     e.printStackTrace();
                     return;
-                }
-
-                if (con != null) {
-                    Log.d(LOG_TAG,"You made it, take control your database now!");
-                } else {
-                    Log.d(LOG_TAG,"Failed to make connection!");
                 }
             }
         });
