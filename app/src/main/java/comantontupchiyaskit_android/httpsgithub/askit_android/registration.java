222c3f34d7966622d8e2ca93cd48e1b235497f3e
tender7morning <SaNNy2009@ukr.net>
2015-04-23 18:15:44 +0300

    Backgroung change

---
+++ app/src/main/java/comantontupchiyaskit_android/httpsgithub/askit_android/registration.java
@@ -0,0 +1,15 @@
+package comantontupchiyaskit_android.httpsgithub.askit_android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Anna on 4/20/2015.
+ */
+public class registration extends Activity {
+
+
+
+
+}
+
